dc869dd52b22476ac20f19c41754037d67f717df
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-11-21 21:29:54 +0530

    Add files via upload

--- webpackBabel_setup/src/index.js
+++ webpackBabel_setup/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Img from "./setupDemo.jpg";
+import "./styles.scss";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello!</h1>
+      <h1>Hello World!</h1>
+      <img src={Img} />
+      <p>Styling using sass</p>
     </div>
   );
 };
